15ef8e96d62816c7d2c783ce1bbb6eb29f87ddca
oldlepa <odiolove86@yahoo.fr>
2017-07-18 20:45:26 +0200

    New: Initialisation du projet

---
+++ gestion-logement/src/main/java/gestionLogement/gestion_logement/App.java
@@ -0,0 +1,13 @@
+package gestionLogement.gestion_logement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
